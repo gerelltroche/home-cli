4da69cdc5d07b6390c042bcc397f963da0c53da8
Gerell T <gerelltroche@gmail.com>
2022-03-31 14:40:13 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+// git checkout develop
+// git pull origin develop
+// check version in ./package.json
+// new_version = version + 1
+// git flow release start $new_version
+// change version to $new_version in ./package.json
+// git commit -m "Bump to version $new_version"
+// git flow release finish $new_version -m 'v$new_version' <-- test this
+
